12b0889cce3779b03a61208f9c97d9f7f50811c3
nicolinopaganelli <54078029+nicolinopaganelli@users.noreply.github.com>
2020-01-23 14:43:54 -0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,10 +1,12 @@
 # BMI Calculator
 ## BMI = (weight in lbs * 703) / (height in inches)^2
+#### is this ugly code? yes. is the user interface even uglier? also yes. did I write this in 10 minutes because 
+#### I didn't feel like using an online BMI calculator? absolutely.
 
 def main():
     INCH = input('how tall are you?\n(in inches)')
-    LBS = input('how much do you weigh?\(in pounds)')
-    print(calculate(INCH, LBS))
+    LBS = input('how much do you weigh? (in pounds)')
+    print('your BMI is', calculate(INCH, LBS))
 
 def calculate(INCH, LBS):
     bmi = (int(LBS) * 703) / (int(INCH) ** 2)
